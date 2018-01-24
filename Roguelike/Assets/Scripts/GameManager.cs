74f8cac140de01115327e813431346ca8ef87018
panzhiyi3 <panzhiyi3@gmail.com>
2018-01-24 21:32:10 +0800

    update

---
+++ Roguelike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    void Update()
+    {
+
+    }
+}
